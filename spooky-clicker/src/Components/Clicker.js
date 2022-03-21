fc4c6fa98ae8c00c2ff1c505e1b6fb1c52f7c441
Carrie Platten <carriep@Carries-MacBook-Pro.local>
2022-03-21 12:34:18 +0000

    spooky energy counter now updates

--- spooky-clicker/src/Components/Clicker.js
+++ spooky-clicker/src/Components/Clicker.js
@@ -4,16 +4,26 @@ import ghostButton from "../ghost.png";
 export default class Clicker extends Component {
   constructor(props) {
     super(props);
-    this.state = { ghosts: 0 };
+    this.state = { ghosts: 0, spookyEnergy: 0 };
   }
 
   handleGhostClick = () => {
-    this.setState(
-      (currState) => {
-        return { ghosts: currState.ghosts + 1 };
-      }
-    );
+    this.setState((currState) => {
+      return { ghosts: currState.ghosts + 1 };
+    });
   };
+
+  increaseSpookyEnergy = () => {
+    this.setState((currState) => {
+      return { spookyEnergy: currState.spookyEnergy + 1 };
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.ghosts < this.state.ghosts) {
+        this.increaseSpookyEnergy()
+    }
+  }
 
   render() {
     return (
@@ -23,6 +33,10 @@ export default class Clicker extends Component {
         </button>
         <p>Boo!</p>
         <p>Ghosts: {this.state.ghosts}</p>
+        <section className="resources">
+          <legend>spooky collections</legend>
+          <p>Spooky Energy: {this.state.spookyEnergy}</p>
+        </section>
       </>
     );
   }
